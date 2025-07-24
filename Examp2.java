f3912b92291472e2a9d26d080d479d89fdbc6720
Viswanadham.M <kasimullapudi42@gmail.com>
2025-07-24 12:11:52 +0530

    edited Examp2

--- Examp2.java
+++ Examp2.java
@@ -1,6 +1,6 @@
 public class Examp2 {
     public static void main(String[] args) {
-        System.out.println("Hello from 2");
+        System.out.println("Hello from 2 from feature");
     }
     
 }
